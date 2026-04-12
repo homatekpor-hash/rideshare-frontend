51dd79d5c6ba6618f37f511a81f0cdbf06bf9868
Elis Homatekpor <homatekpor@gmail.com>
2026-04-12 14:55:50 +0100

    Fix import syntax error in RiderDashboard

--- src/pages/RiderDashboard.js
+++ src/pages/RiderDashboard.js
@@ -1,4 +1,5 @@
-import Rimport { sendNotification } from '../utils/notifications';
+import React, { useState, useEffect } from 'react';
+import { sendNotification } from '../utils/notifications';
 eact, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
